aacf27a88ff917bd4d5ef34f149c19ff63650ce0
ALinYiLin <scliuyilin@163.com>
2025-11-24 22:02:52 +0800

    add floating ball and menu route

---
+++ vite-react/src/layout/floating-menu.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import styles from  "./floating-menu.module.css";
+import { routes as ROUTES } from "@/router/routes";
+import type { AppRouteObject } from "@/router";
+import { useNavigate } from "react-router-dom";
+
+function ListMenus({ routes }: { routes: AppRouteObject[] }) {
+  const navigate = useNavigate();
+  const handleClick = (e:React.MouseEvent,path: string) => {
+    e.stopPropagation()
+    navigate(path);
+  };
+  return routes.map((route) => (
+    <div key={route.path} onClick={(e) => handleClick(e,route.path || "")}>
+      {route.handle?.title}
+      {route.children && route.children.length > 0 && (
+        <ListMenus routes={route.children} />
+      )}
+    </div>
+  ));
+}
+
+function FloatingMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles["floating-container"]} >
+      {/* 悬浮球 */}
+      <div className={styles["floating-ball"]} onClick={() => setIsOpen(!isOpen)}></div>
+
+      {/* 菜单 */}
+      {isOpen && (
+        <div className={styles["menu"]}>
+          <ListMenus routes={ROUTES[0].children || []} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FloatingMenu;
