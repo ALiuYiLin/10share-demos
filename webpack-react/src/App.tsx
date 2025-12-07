90f12580193a401b681c641171fe5f49b6403036
ALinYiLin <scliuyilin@163.com>
2025-12-07 17:55:45 +0800

    feat: enhance namespace+BEM utility; refactor to full BEM API; update App and styles; add use-namespace module and index

--- webpack-react/src/App.tsx
+++ webpack-react/src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import styles from "./styles.module.less";
-import { useNameSpace } from "@/utils/namespace";
+import { useNameSpace } from "@/utils";
 
 export default function App() {
-  const {cx,...ns} = useNameSpace(styles, "form");
-  return <div className={cx(ns.b("wrapper"),'nihao')}>Hello React + TypeScript + Webpack</div>;
+  const { cx, ...ns } = useNameSpace(styles, "form");
+  const [active, setActive] = useState(false);
+  return (
+    <div>
+      <div className={cx(ns.b("wrapper"), ns.is("active",active))}>
+        Hello React + TypeScript + Webpack
+      </div>
+      <button className={cx(ns.b("switch"))} onClick={() => setActive(!active)}>
+        Switch
+      </button>
+    </div>
+  );
 }
